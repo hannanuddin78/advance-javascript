da21884757d10ab2e33c3684c47233dbb7379022
hannanuddin78 <67517002+hannanuddin78@users.noreply.github.com>
2020-08-09 18:12:52 +0600

    ture vs false

---
+++ true-false.js
@@ -0,0 +1,34 @@
+const age = 10;
+if (age) {
+    console.log('condition is true '); 
+} else {                                            //condition is true
+    console.log('condition is false ');
+}
+
+const price = 0;
+if (price > 0) {
+    console.log('condition is true '); 
+} else {                                            //condition is false
+    console.log('condition is false ');
+}
+
+let Fname = "raki"
+if (Fname.length > 0) {
+    console.log('condition is true '); 
+} else {                                            //condition is true
+    console.log('condition is false ');
+}
+
+let name = "";
+if (name.length > 0) {
+    console.log('condition is true '); 
+} else {                                            //condition is false
+    console.log('condition is false ');
+}
+
+let SFname;
+if (SFname) {
+    console.log('condition is true '); 
+} else {                                            //condition is false
+    console.log('condition is false ');
+}
