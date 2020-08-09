707f2950c3c2f6fdb9f735aa989c215c6a3e1c65
hannanuddin78 <67517002+hannanuddin78@users.noreply.github.com>
2020-08-09 21:55:21 +0600

    double-vs-triple

---
+++ double-vs-triple-equail.js
@@ -0,0 +1,53 @@
+const first = 10;
+const second = 30;
+if (first == second) {
+    console.log('condition is true');
+}                                                   //condition is false
+else{
+    console.log('condition is false');
+}
+
+let one = 10;
+let two = 10;
+if (one == two) {
+    console.log('condition is true');
+}                                                   //condition is true
+else{
+    console.log('condition is false');
+}
+
+let a = 10;
+let b = '10';
+if (a == b) {
+    console.log('condition is true');
+}                                                   //condition is true
+else{
+    console.log('condition is false');
+}
+
+let patOom = 10;
+let ditIwo = 10;
+if (patOom === ditIwo) {
+    console.log('condition is true');
+}                                                   //condition is true
+else{
+    console.log('condition is false');
+}
+
+let patOomOne = 10;
+let ditIwoTwo = 20;
+if (patOomOne === ditIwoTwo) {
+    console.log('condition is true');
+}                                                   //condition is false
+else{
+    console.log('condition is false');
+}
+
+let x = 10;
+let y = '10';
+if (x === y) {
+    console.log('condition is true');
+}                                                   //condition is false
+else{
+    console.log('condition is false');
+}
